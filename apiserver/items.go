80e5dfa19befce7c017dd92be82e96338f83507d
don-galak <ptagalac@gmail.com>
2023-12-11 22:01:30 +0200

    add items endpoints

---
+++ apiserver/items.go
@@ -0,0 +1,11 @@
+package apiserver
+
+import "net/http"
+
+func (s *APIServer) createItem(w http.ResponseWriter, req *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) listItems(w http.ResponseWriter, req *http.Request) error {
+	return nil
+}
